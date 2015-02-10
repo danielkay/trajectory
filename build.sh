2da8bf829dd751e4f84491bccfb3c324764f330a
Daniel Kay <contact@danielkay.co.uk>
2015-02-10 20:20:09 +0000

    updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup | bash - 
-yum install -y nodejs
 php -r "readfile('https://getcomposer.org/installer');" | php 
 php composer.phar install
-npm install bower
