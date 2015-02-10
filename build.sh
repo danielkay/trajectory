ce47c4c77b55f75f4be4b58d3c3ace445e46d25f
Daniel Kay <contact@danielkay.co.uk>
2015-02-10 20:14:04 +0000

    updated build script

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-apt-get install curl
 curl -sL https://deb.nodesource.com/setup | bash - 
-apt-get install -y nodejs 
+yum install -y nodejs
 php -r "readfile('https://getcomposer.org/installer');" | php 
 php composer.phar install
 npm install bower
