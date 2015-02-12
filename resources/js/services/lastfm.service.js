d8e88202e22dd354ea8571b032a4c3f65f460f8e
Daniel Kay <contact@danielkay.co.uk>
2015-02-12 02:48:49 +0000

    fixed lastfmService

--- resources/js/services/lastfm.service.js
+++ resources/js/services/lastfm.service.js
@@ -1,6 +1,8 @@
+var BASE_URL = "http://ws.audioscrobbler.com/2.0/?api_key=198aaad97ed3a7ae97043ccc5a327ba6&format=json";
+
 angular.module('lastfmService', ['ngResource'])
-	.factory('artistSearch', function(apiUrl, $resource) {
-		return $resource(apiUrl + '&method=artist.search&artist=:artist', {}, {
+	.factory('artistSearch', function($resource) {
+		return $resource(BASE_URL + '&method=artist.search&artist=:artist', {}, {
 			query: { method: 'GET', params: {}, isArray: false }
 		});
 	});
