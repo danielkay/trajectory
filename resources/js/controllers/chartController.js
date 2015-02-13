28904e3cc9ea97b46be3666e1953fc070e873afc
Daniel Kay <contact@danielkay.co.uk>
2015-02-13 02:23:16 +0000

    added parseLastFmData function (D.R.Y)

--- resources/js/controllers/chartController.js
+++ resources/js/controllers/chartController.js
@@ -2,19 +2,12 @@ app.controller('chartController', function($scope, artistSearch, artistGetSimila
     $scope.artistSearch = function() {
         var res = artistSearch.query({artist: $scope.searchTerm}, function () {
             if(res.results) {
-                $scope.lastFmArtists = res.results.artistmatches.artist;
-                var artistArray = [];
-
-                angular.forEach($scope.lastFmArtists, function(value, key) {
-                    var artist = {
-                        name: value['name'],
-                        size: value['listeners'],
-                        image: value['image'][0]['#text']
-                    }
-                    artistArray.push(artist);
-                });
-                $scope.searchType = 'artist';
-                $scope.artistData = { name: "lastFmData", children: artistArray };
+                var artistArray = parseLastFmData(res.results.artistmatches.artist);
+                
+                if(artistArray.length) {
+                    $scope.searchType = 'artist';
+                    $scope.artistData = { name: "lastFmData", children: artistArray };
+                }
             }
         });
     };
@@ -22,20 +15,53 @@ app.controller('chartController', function($scope, artistSearch, artistGetSimila
     $scope.artistGetSimilar = function() {
         var res = artistGetSimilar.query({artist: $scope.searchTerm}, function () {
             if(res.similarartists) {
-                $scope.lastFmArtists = res.similarartists.artist;
-                var artistArray = [];
-
-                angular.forEach($scope.lastFmArtists, function(value, key) {
-                    var artist = {
-                        name: value['name'],
-                        size: value['match'],
-                        image: value['image'][0]['#text']
-                    }
-                    artistArray.push(artist);
-                });
-                $scope.searchType = 'similar';
-                $scope.artistData = { name: "lastFmData", children: artistArray };
+                var artistArray = parseLastFmData(res.similarartists.artist);
+                if(artistArray.length) {
+                    $scope.searchType = 'similar';
+                    $scope.artistData = { name: "lastFmData", children: artistArray };
+                }
             }
         });
+    };
+    
+    function parseLastFmData(data) {
+        var artistArray = [];
+
+        if(data && data.image) {
+            var artist = {
+                name: data.name,
+                size: (data.match) ? data.match : data.listeners
+            };
+
+            if(data.image)
+                artist['image'] = data.image[0]['#text'];
+
+            artistArray.push(artist);
+        } else {
+            angular.forEach(data, function(value, key) {
+                var artist = {
+                    name: value['name'],
+                    size: (value['match']) ? value['match'] : value['listeners']
+                };
+
+                if(value['image'])
+                    artist['image'] = value['image'][0]['#text'];
+
+                artistArray.push(artist);
+            });
+        }
+        
+        return artistArray;
+    }
+    
+    $scope.searchAgain = function() {
+        switch($scope.searchType) {
+            case 'artist':
+                $scope.artistSearch();
+                break;
+            case 'similar':
+                $scope.artistGetSimilar();
+                break;
+        }
     };
 });
